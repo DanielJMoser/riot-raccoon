b4fb325cd5d30ba12c1da3a2d627873659adc138
DanielJMoser <daniel.moser@mci.edu>
2025-03-24 15:34:37 +0100

    Save the orders to backend

--- backend/services/sanityClient.ts
+++ backend/services/sanityClient.ts
@@ -6,7 +6,8 @@ const client = sanityClient({
     projectId: 'l6v56id4',
     dataset: 'production',        // or your chosen dataset name
     apiVersion: '2023-03-01',     // use the latest API version
-    useCdn: true,                 // set to `false` for real-time data
+    token: 'sklK22Xj5wMVdyxTb1OUUM1XcPoDtOnuisIvQWmZVj3buBZvo1KBNkQPMrvctVW5cza5GdjcYMNhQFsKkjFaQ2OE9lVvdlsGt9xMjabAcAivZi6ffoW77Q85ncK83zBROsT6ebBFdYRNeb7zRa8pQZYFnsY5vb8mJY7RwXxDJtOdtaDgFsf8',
+    useCdn: false,                 // set to `false` for real-time data
 });
 
 // Set up a helper function for generating image URLs
