9626c789c904e8fff3f6347eba440041b47e6307
DanielJMoser <daniel.moser@mci.edu>
2025-03-31 16:13:27 +0200

    e

--- backend/services/sanityClient.ts
+++ backend/services/sanityClient.ts
@@ -3,18 +3,14 @@ import imageUrlBuilder from '@sanity/image-url';
 import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
 const client = sanityClient({
-    projectId: process.env.PROJECT_ID,
-    dataset: 'production',        // or your chosen dataset name
-    apiVersion: '2023-03-01',     // use the latest API version
-    token: process.env.SANITY_API_TOKEN_RW,
-    useCdn: false,                 // set to `false` for real-time data
+    projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+    dataset: 'production',
+    apiVersion: '2023-03-01',
+    token: import.meta.env.VITE_SANITY_API_TOKEN,
+    useCdn: false,
 });
 
-// Set up a helper function for generating image URLs
 const builder = imageUrlBuilder(client);
 
-export const urlFor = (source: SanityImageSource) => {
-    return builder.image(source);
-};
-
+export const urlFor = (source: SanityImageSource) => builder.image(source);
 export default client;
